fe3ce96bcc363ebcde1ae7642adb1c2427e3c9b2
Nikimedinagit <ignaciomedinagit@gmail.com>
2026-08-13 21:56:45 -0300

    Implementacion de la clase Proveedor, con su respectiva Dto y Migracion.

---
+++ Concesionaria.Application/DTOs/Personas/ProveedorDto.cs
@@ -0,0 +1,11 @@
+public class ProveedorDto
+{
+    public Guid ProveedorId { get; set; }
+    public string Nombre { get; set; }
+    public string Cuil { get; set; }
+    public string Telefono { get; set; }
+    public string Email { get; set; }
+    public string Domicilio { get; set; }
+    public string Servicio { get; set; }
+    public string Observacion { get; set; }
+}
